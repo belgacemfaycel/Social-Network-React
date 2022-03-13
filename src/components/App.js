49ff9dd6f34fa8cc884fdfd21d1d786afc7a427d
houris <faycelbelgacem93@gmail.com>
2022-03-13 20:47:40 +0100

    add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 
 import '../styles/App.css';
  import Header from './Header'; 
+ import Footer from './Footer'
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
 			<Header>
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Header>
+      <div></div>
+      <Footer />
     </div>
   );
 }
